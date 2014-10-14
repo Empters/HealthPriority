9e87b6d1f55a5c61c97a5d194ae1a7b986c38557
bogi <bogoi.bogdanov@gmail.com>
2014-10-14 23:50:12 +0300

    Initial files added, build broken

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,8 @@
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
 # It's strongly recommended that you check this file into your version control system.
-ActiveRecord::Schema.define(version: 20141012190436) do
 
+ActiveRecord::Schema.define(version: 20141012190436) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
