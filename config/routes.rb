27b8ccc08078257685681e578eb5bf5e8eed55fd
nikolabogdanov <nikola.k.bogdanov@gmail.com>
2014-10-20 23:58:52 +0300

    login modals created

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   ActiveAdmin.routes(self)
   root to: "home#index"
 
-  devise_for :users, controllers: { sessions: "users/sessions" }
+  devise_for :users, controllers: { sessions: "users/sessions", registrations: "users/registrations" }
 end
