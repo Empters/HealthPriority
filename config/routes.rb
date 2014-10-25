87f220e0bca1a10d808d09f3f4a845b7ff9a7d17
nikolabogdanov <nikola.k.bogdanov@gmail.com>
2014-10-25 12:10:44 +0300

    Adding links to product page from home product list, seeds for products are created

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  root to: "home#index"
 
-  # get "/users/sign_in", to: "home#index"
+  root to: "home#index"
 
   devise_for :users, controllers: { sessions: "users/sessions", registrations: "users/registrations", passwords: "users/passwords", confirmations: "users/confirmations" }
 
+  resources :products
 end
