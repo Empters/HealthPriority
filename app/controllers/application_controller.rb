691adc97f04f33b798d1496cb72703523c475e35
bogi <bogoi.bogdanov@gmail.com>
2014-10-30 21:08:38 +0200

    Categories loaded at proper place

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :set_categories
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  private
+  def set_categories
+    @categories = Category.top_level
+  end
 end
