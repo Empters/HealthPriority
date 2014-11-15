36c01b9af47ae5ac7de3da9b3781d47be234b077
gmanchev <drakstur@gmail.com>
2014-11-15 19:52:51 +0200

    Create base product partial view - use in last visit products and grid with products. Remember last visit products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -14,8 +14,8 @@ class ProductsController < ApplicationController
   # GET /products/1
   # GET /products/1.json
   def show
-    add_breadcrumb 'Products', :products_path
-
+    add_breadcrumb 'Products', :products_pat
+    add_to_last_visit_product(@product)
   end
 
   # GET /products/new
@@ -82,7 +82,7 @@ class ProductsController < ApplicationController
     respond_to do |format|
       format.js {
         if request.xhr?
-            render 'search.js.erb'
+          render 'search.js.erb'
         end
       }
       format.html {
@@ -104,46 +104,64 @@ class ProductsController < ApplicationController
 
     respond_to do |format|
       format.html { puts "html" }
-      format.js { render 'search.js.erb'  }
+      format.js { render 'search.js.erb' }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_product
-      @product = Product.find(params[:id])
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_product
+    @product = Product.find(params[:id])
+  end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def product_params
-      params.require(:product).permit(:name, :model, :quantity, :viewed, :image, :price, :points, :description)
-    end
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def product_params
+    params.require(:product).permit(:name, :model, :quantity, :viewed, :image, :price, :points, :description)
+  end
 
   def search_params
     params.permit(:token, :format, :category)
   end
 
   def search_and_filter(category, token)
-    if(!token && !category)
+    if (!token && !category)
       $products = $products ||= Product.all
     elsif !category
       $products = Product.where("UPPER(name) LIKE :name", {:name => "%" + token.upcase + "%"})
     elsif !token
       @category = Category.find(search_params[:category])
-      if(@category.parent)
-       $products = Product.joins(products_categories: :category).where(categories: {id: @category})
+      if (@category.parent)
+        $products = Product.joins(products_categories: :category).where(categories: {id: @category})
       else
-       $products = Product.joins(products_categories: :category).where('(categories.id = :category or categories.parent_id = :category)',
-                                                                       {:category => @category})
+        $products = Product.joins(products_categories: :category).where('(categories.id = :category or categories.parent_id = :category)',
+                                                                        {:category => @category})
       end
     else
       @category = Category.find(search_params[:category])
-      if(@category.parent)
+      if (@category.parent)
         $products = Product.joins(products_categories: :category).where(categories: {id: @category}).where('UPPER(products.name) LIKE :token', {token: "%" + token.upcase + "%"})
       else
         $products = Product.joins(products_categories: :category).where('(categories.id = :category or categories.parent_id = :category)',
-                                                                    {:category => @category}).where('UPPER(products.name) LIKE :token', {token: "%" + token.upcase + "%"})
+                                                                        {:category => @category}).where('UPPER(products.name) LIKE :token', {token: "%" + token.upcase + "%"})
       end
+    end
+  end
+
+  def add_to_last_visit_product(product)
+    # Check input parameters
+    if product.nil?
+      return
+    end
+
+    # Change last visit products
+    if $last_visit_products.nil?
+      $last_visit_products = Array.new
+      $last_visit_products << product
+    else
+      if $last_visit_products.length > 3
+        $last_visit_products.delete_at($last_visit_products.length - 1)
+      end
+      $last_visit_products.unshift(product)
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,11 @@ Rails.application.routes.draw do
 
   ActiveAdmin.routes(self)
 
+  # Init active admin
   devise_for :admin_users, ActiveAdmin::Devise.config
+
+  # Init login gem
+  devise_for :users, controllers: { sessions: "users/sessions", registrations: "users/registrations", passwords: "users/passwords", confirmations: "users/confirmations" }
 
   get 'delivery', to: 'delivery#index'
   get 'faq', to: 'faq#index'
@@ -25,5 +29,4 @@ Rails.application.routes.draw do
   resources :shopping_cards, only: :index
   resources :products
 
-  devise_for :users, controllers: { sessions: "users/sessions", registrations: "users/registrations", passwords: "users/passwords", confirmations: "users/confirmations" }
 end
