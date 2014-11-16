1f290cbb1b6745ac6b565af0778b4c60afbecfa1
nikolabogdanov <nikola.k.bogdanov@gmail.com>
2014-11-16 13:27:09 +0200

    move logic from product controller to search module

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -72,16 +72,9 @@ class ProductsController < ApplicationController
 
   # GET /products/search
   def search
-    # if search_params[:token]
-    #   $products = Product.where("UPPER(name) LIKE :name", {:name => "%" + search_params[:token].upcase + "%"})
-    # else
-    #   $products ||= Product.all
-    # end
-
     search_and_filter(search_params[:category], search_params[:token])
-
-    # search_and_filter(@category, '')
-
+    set_pages
+    $products = $products.paginate(:page => @current_page, :per_page => 6)
     respond_to do |format|
       format.js {
         if request.xhr?
@@ -128,47 +121,5 @@ class ProductsController < ApplicationController
   # Never trust parameters from the scary internet, only allow the white list through.
   def product_params
     params.require(:product).permit(:name, :model, :quantity, :viewed, :image, :price, :points, :description)
-  end
-
-  def search_and_filter(category, token)
-    if (!token && !category)
-      $products = $products ||= Product.all
-    elsif !category
-      $products = Product.where("UPPER(name) LIKE :name", {:name => "%" + token.upcase + "%"})
-    elsif !token
-      @category = Category.find(search_params[:category])
-      if (@category.parent)
-        $products = Product.joins(products_categories: :category).where(categories: {id: @category})
-      else
-        $products = Product.joins(products_categories: :category).where('(categories.id = :category or categories.parent_id = :category)',
-                                                                        {:category => @category})
-      end
-    else
-      @category = Category.find(search_params[:category])
-      if (@category.parent)
-        $products = Product.joins(products_categories: :category).where(categories: {id: @category}).where('UPPER(products.name) LIKE :token', {token: "%" + token.upcase + "%"})
-      else
-        $products = Product.joins(products_categories: :category).where('(categories.id = :category or categories.parent_id = :category)',
-                                                                        {:category => @category}).where('UPPER(products.name) LIKE :token', {token: "%" + token.upcase + "%"})
-      end
-    end
-  end
-
-  def add_to_last_visit_product(product)
-    # Check input parameters
-    if product.nil?
-      return
-    end
-
-    # Change last visit products
-    if $last_visit_products.nil?
-      $last_visit_products = Array.new
-      $last_visit_products << product
-    else
-      if $last_visit_products.length > 3
-        $last_visit_products.delete_at($last_visit_products.length - 1)
-      end
-      $last_visit_products.unshift(product)
-    end
   end
 end
