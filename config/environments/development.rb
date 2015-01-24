13ce8353951a479ce7c0db893a200fd448bbea76
nikolabogdanov <nikola.k.bogdanov@gmail.com>
2015-01-24 14:33:35 +0200

    more text

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -75,5 +75,5 @@ Rails.application.configure do
   #config.allow_concurrency = true
   #Setting default language
   config.i18n.enforce_available_locales = true
-  config.i18n.default_locale = :bg
+  config.i18n.default_locale = :en
 end
