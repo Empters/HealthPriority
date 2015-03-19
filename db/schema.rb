6921cdb48910f42c918bdff889289c544c3d496c
nikolabogdanov <nikola.k.bogdanov@gmail.com>
2015-03-19 15:05:42 +0200

    add payment_id to orders

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150319124931) do
+ActiveRecord::Schema.define(version: 20150319130354) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -158,6 +158,7 @@ ActiveRecord::Schema.define(version: 20150319124931) do
     t.string   "ipn_track_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "payment_id"
   end
 
   create_table "payments", force: true do |t|
