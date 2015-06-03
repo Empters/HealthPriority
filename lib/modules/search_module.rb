3a0d7d489c398e63cc7507c678ea8f63e8813b62
gmanchev <drakstur@gmail.com>
2015-06-03 15:15:31 +0300

    Search products route changes

--- lib/modules/search_module.rb
+++ lib/modules/search_module.rb
@@ -8,41 +8,39 @@ module Modules
     # TODO - create multi filter method
     def search_and_filter()
 
-      category = session[:category]
-      token = session[:token]
-
-      if (session[:range])
-        @products = Product.where(price: @price_ranges[session[:range].to_i][0] .. @price_ranges[session[:range].to_i][1]);
-      elsif (session[:brand])
-        @products = Product.where('manufacturer_id' => session[:brand])
-      elsif (!token && !category)
+      if (!@range.blank?)
+        @products = Product.where(price: @price_ranges[@range.to_i][0] .. @price_ranges[@range.to_i][1]);
+      elsif (!@brand.blank?)
+        @products = Product.where('manufacturer_id' => @brand)
+      elsif (@token.blank? && @category.blank?)
         @products ||= Product.all
-      elsif category.blank?
-        @products = Product.where('UPPER(name) LIKE :name or UPPER(description) LIKE :name ', {:name => "%#{token.upcase}%"})
-      elsif token.blank?
-        @category = Category.find(category)
-        if (@category.parent)
-          @products = Product.joins(products_categories: :category).where(categories: {id: @category})
-        else
-          @products = Product.joins(products_categories: :category).where('(categories.id = :category or categories.parent_id = :category)',
-                                                                          {:category => @category})
-        end
+      elsif @category.blank?
+        @products = Product.where('UPPER(name) LIKE :name or UPPER(description) LIKE :name ', {:name => "%#{@token.upcase}%"})
       else
-        @category = Category.find(search_params[:category])
-        if (@category.parent)
-          @products = Product.joins(products_categories: :category).where(categories: {id: @category}).where('UPPER(products.name) LIKE :token', {token: '%' + token.upcase + '%'})
+        @categoryObj = Category.find(@category)
+        if @token.blank?
+          if (@categoryObj.parent)
+            @products = Product.joins(products_categories: :category).where(categories: {id: @categoryObj})
+          else
+            @products = Product.joins(products_categories: :category).where('(categories.id = :category or categories.parent_id = :category)',
+                                                                            {:category => @categoryObj})
+          end
         else
-          @products = Product.joins(products_categories: :category).where('(categories.id = :category or categories.parent_id = :category)',
-                                                                          {:category => @category}).where('UPPER(products.name) LIKE :token', {token: '%' + token.upcase + '%'})
+          if (@categoryObj.parent)
+            @products = Product.joins(products_categories: :category).where(categories: {id: @categoryObj}).where('UPPER(products.name) LIKE :token', {token: '%' + @token.upcase + '%'})
+          else
+            @products = Product.joins(products_categories: :category).where('(categories.id = :category or categories.parent_id = :category)',
+                                                                            {:category => @categoryObj}).where('UPPER(products.name) LIKE :token', {token: '%' + @token.upcase + '%'})
+          end
         end
       end
     end
 
     def set_search_and_filter_params
-      session[:token] = search_params[:token]
-      session[:category] = search_params[:category]
-      session[:brand] = search_params[:brand]
-      session[:range] = search_params[:range]
+      @token = search_params[:token]
+      @category = search_params[:category]
+      @brand = search_params[:brand]
+      @range = search_params[:range]
     end
 
   end
